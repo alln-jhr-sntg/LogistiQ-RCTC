144219c822e4f7ea7a0699590f66a350658c9320
alln-jhr-sntg <allenjherussantiago@gmail.com>
2026-08-14 17:05:06 +0800

    Capitalization

--- views/auth/login.php
+++ views/auth/login.php
@@ -36,8 +36,8 @@
             </form>
             <?php if (!empty($demoAccounts)): ?>
             <div class="demo-accounts">
-                <strong>Demo accounts <br>
-                password: <code>password</code></strong>
+                <strong>Demo Accounts <br>
+                Password: <code>password</code></strong>
                 <?php foreach ($demoAccounts as $account): ?>
                 <div class="demo-accounts-row">
                     <span><?= Helpers::e($account['label']) ?>:</span>
